ea91ecc7eb8a85204b1fe10735d300614c59c059
Logan Walmsley <lwalmsley@kalibre.com.au>
2022-09-20 09:25:57 +1000

    commit message

---
+++ GW2-DonBot/Models/BotSecretsDataModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Testing.Models
+{
+    using Newtonsoft.Json;
+
+    public class BotSecretsDataModel
+    {
+        [JsonProperty("scrapedUrl")]
+        public string ScrapedUrl { get; set; }
+
+        [JsonProperty("webhookUrl")]
+        public string WebhookUrl { get; set; }
+
+        [JsonProperty("pingedUser")]
+        public string PingedUser { get; set; }
+    }
+}

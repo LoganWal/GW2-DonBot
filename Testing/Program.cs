be920a337c8e62fde2ac542d22c9d29e78761dce
Logan <Logan.Walmsley@protonmail.com>
2022-09-20 00:59:39 +1000

    commit message

---
+++ Testing/Program.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.Webhook;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Testing.Models;
+
+var html = @"";
+HtmlWeb web = new HtmlWeb();
+var htmlDoc = web.Load(html);
+var logDataStartIndex = htmlDoc.ParsedText.IndexOf("logData") + 10;
+var logDataEndIndex = htmlDoc.ParsedText.IndexOf("};");
+
+var data = htmlDoc.ParsedText.Substring(logDataStartIndex, (logDataEndIndex - logDataStartIndex) + 1);
+
+var parsedData = JsonConvert.DeserializeObject<EliteInsightDataModel>(data);
+
+var players = string.Join(Environment.NewLine, parsedData?.Players?.ToList()?.Select(player => $"{player?.Name}"));
+
+DiscordWebhook hook = new DiscordWebhook();
+hook.Url = "";
+
+DiscordMessage message = new DiscordMessage();
+message.Content = $"ping <@225132265762586625> \n```Latest players: \n{players}```";
+message.TTS = false;
+message.Username = "GW2-DonBot";
+message.AvatarUrl = "https://i.imgur.com/tQ4LD6H.png";
+
+hook.Send(message);

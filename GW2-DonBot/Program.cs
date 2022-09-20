a0d5bc526b2872fc20c8449e0cbd4768904c6c18
Alexa O'Brien <elinguate.dev@gmail.com>
2022-09-20 12:24:05 +1200

    added basic formatted message
    Setup a basic formatted message that has some of the properties exposed we want: player counts, damage dealt, DPS, downs, deaths. Added a StringExtension to do padding to center.

--- GW2-DonBot/Program.cs
+++ GW2-DonBot/Program.cs
@@ -27,7 +27,64 @@ var data = htmlDoc.ParsedText.Substring(logDataStartIndex, (logDataEndIndex - lo
 var parsedData = JsonConvert.DeserializeObject<EliteInsightDataModel>(data);
 
 // Building the actual message to be sent
-var players = string.Join(Environment.NewLine, parsedData?.Players?.ToList()?.Select(player => $"{player?.Name}"));
+var content = "";
+var playerNameList = string.Join(Environment.NewLine, parsedData?.Players?.ToList()?.Select(player => $"{player?.Name}"));
+var friendlyCount = parsedData?.Players?.Length;
+var friendlyDamage = parsedData?.Players?.ToList()?.Sum(player => player.Details.DmgDistributions[0].ContributedDamage);
+var friendlyDPS = (float)friendlyDamage / (float)friendlyCount;
+var enemyCount = parsedData?.Targets?.Length;
+var enemyDamage = parsedData?.Targets?.ToList()?.Sum(player => player.Details.DmgDistributions[1].ContributedDamage);
+var enemyDPS = (float)enemyDamage / (float)enemyCount;
+
+/* -- Exploring the offensive stats breakdown
+var phase = parsedData?.Phases[1];
+var players = parsedData?.Players;
+string test2 = "";
+for (int i = 0; i < players.Length; i++)
+{
+    test2 += $"{players[i].Name}:\n";
+    
+    for (int x = 0; x < phase.OffensiveStatsTargets[i].Length; x++)
+    {
+        for (int y = 0; y < phase.OffensiveStatsTargets[i][x].Length; y++)
+        {
+            test2 += $"[{x},{y}]: ";
+            test2 += $"{phase.OffensiveStatsTargets[i][x][y]}";
+            test2 += $"\n";
+        }
+    }
+}
+*/
+
+var friendlyDowns = parsedData?.Phases[1].DefStats.ToList().Sum(playerDefStats => playerDefStats[8].Integer);
+var friendlyDeaths = parsedData?.Phases[1].DefStats.ToList().Sum(playerDefStats => playerDefStats[10].Integer);
+
+var enemyDowns = parsedData?.Phases[1].OffensiveStatsTargets.ToList().Sum(playerOffStats => playerOffStats[0][13]);
+var enemyDeaths = parsedData?.Phases[1].OffensiveStatsTargets.ToList().Sum(playerOffStats => playerOffStats[0][12]);
+
+var friendlyCountStr = friendlyCount?.ToString().PadCenter(7);
+var friendlyDamageStr = friendlyDamage?.ToString().PadCenter(7);
+var friendlyDPSStr = friendlyDPS.ToString("F0").PadCenter(7);
+var friendlyDownsStr = friendlyDowns?.ToString().PadCenter(7);
+var friendlyDeathsStr = friendlyDeaths?.ToString().PadCenter(7);
+
+var enemyCountStr = enemyCount?.ToString().PadCenter(7);
+var enemyDamageStr = enemyDamage?.ToString().PadCenter(7);
+var enemyDPSStr = enemyDPS.ToString("F0").PadCenter(7);
+var enemyDownsStr = enemyDowns?.ToString().PadCenter(7);
+var enemyDeathsStr = enemyDeaths?.ToString().PadCenter(7);
+
+content += $"Have a look at my new report! <@{secrets.PingedUser}>\n";
+content += $"**Friends:**";
+content += $"```Players  Damage     DPS     Downs   Deaths \n";
+content +=    $"-------  -------  -------  -------  -------\n";
+content +=    $"{friendlyCountStr}  {friendlyDamageStr}  {friendlyDPSStr}  {friendlyDownsStr}  {friendlyDeathsStr}```";
+content += $"**Enemies:**";
+content += $"```Players  Damage     DPS     Downs   Deaths \n";
+content +=    $"-------  -------  -------  -------  -------\n";
+content +=    $"{enemyCountStr}  {enemyDamageStr}  {enemyDPSStr}  {enemyDownsStr}  {enemyDeathsStr}```";
+content += $"**Players Found:**";
+content += $"```{playerNameList}```";
 
 // Prepping webhook
 DiscordWebhook hook = new DiscordWebhook();
@@ -35,7 +92,7 @@ hook.Url = secrets.WebhookUrl;
 
 // Building the message
 DiscordMessage message = new DiscordMessage();
-message.Content = $"ping <@{secrets.PingedUser}> \n```Latest players: \n{players}```";
+message.Content = content;
 message.TTS = false;
 message.Username = "GW2-DonBot";
 message.AvatarUrl = "https://i.imgur.com/tQ4LD6H.png";
---
+++ GW2-DonBot/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class StringExtensions
+{    
+    public static string PadCenter(this string str, int length)
+    {
+        return str.PadLeft(((length - str.Length) / 2 + str.Length)).PadRight(length);
+    }
+}

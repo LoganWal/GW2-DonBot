a001700b6536369ecd64daf2ad169258079175e8
Logan <Logan.Walmsley@protonmail.com>
2022-10-11 00:34:55 +1000

    file rework

--- GW2-DonBot/Services/SecretsServices/SecretServices.cs
+++ GW2-DonBot/Services/SecretsServices/SecretServices.cs
@@ -1,39 +1,17 @@
-﻿using GW2DonBot.Models;
-using GW2DonBot.Models.Statics;
-using Models;
-using Services.CacheServices;
-using Services.FileServices;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace Services.SecretsServices
 {
     public class SecretServices : ISecretService
     {
-        private readonly ICacheService _cacheService;
 
-        private readonly IFileService _fileService;
-
-        public SecretServices(ICacheService cacheService, IFileService fileService)
+        public T? FetchBotAppSettings<T>(string key) where T: class
         {
-            _cacheService = cacheService;
-            _fileService = fileService;
-        }
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-        public async Task<BotSecretsDataModel> FetchBotAppSettings()
-        {
-            var appsettings = _cacheService.Get<BotSecretsDataModel>(CacheKey.Secrets);
-            if (appsettings == null)
-            {
-                appsettings = await _fileService.ReadAndParse<BotSecretsDataModel>(FileLocation.AppSettings);
-
-                if (appsettings == null)
-                {
-                    throw new Exception("Appsettings are null");
-                }
-
-                _cacheService.Set(CacheKey.Secrets, appsettings, DateTimeOffset.Now.AddMinutes(2));
-            }
-
-            return appsettings;
+            return config[key] as T;
         }
     }
 }
